e8b101c52208abff5143a7026341300a819b0796
pareshmeher <67202746+pareshmeher@users.noreply.github.com>
2021-02-09 19:44:26 +0530

    Add files via upload

---
+++ digital_clock.py
@@ -0,0 +1,18 @@
+from tkinter import *
+from time import strftime
+
+root = Tk()
+root.title("Digital Clock")
+
+def time():
+    string = strftime("%H:%M:%S %p")
+    lbl.config(text = string)
+    lbl.after(1000, time)
+
+lbl = Label(root, font = ("Arial", 160, "bold"), bg="black", fg = "white")
+
+lbl.pack(anchor = "center", fill= "both", expand= 1)
+
+time()
+
+mainloop()
